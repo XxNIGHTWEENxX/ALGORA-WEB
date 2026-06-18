d6edc745e6fce8ee94e22576b7d0abddad4c3b57
XxNIGHTWEENxX <alan.garciagon@alumno.buap.mx>
2026-06-17 18:32:44 -0600

    landing: cubre toda la plataforma - asistente IA Aura, lealtad y monedero, marketing, modulos restaurante y gimnasio, bundles Todo+RES y Todo+GYM, tabla comparativa ampliada

--- git-push.bat
+++ git-push.bat
@@ -2,7 +2,7 @@
 cd /d "%~dp0"
 del /f /q .git\index.lock 2>nul
 git add -A
-git commit -m "landing: tabla comparativa de planes (Lite vs POS vs Todo ALGORA) por categorias con badge POPULAR; nuevo posicionamiento una sola plataforma, logo real, botones a algorapos.com.mx, plan Lite, SEO"
+git commit -m "landing: cubre toda la plataforma - asistente IA Aura, lealtad y monedero, marketing, modulos restaurante y gimnasio, bundles Todo+RES y Todo+GYM, tabla comparativa ampliada"
 git push origin main
 echo.
 echo Done! Press any key to close...
