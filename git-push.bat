f7eca21e777dddebf4a80fd7ace1f263afa60b4c
XxNIGHTWEENxX <alan.garciagon@alumno.buap.mx>
2026-07-01 03:58:20 -0600

    Precios: Agenda baja a 149 y bundle Todo ALGORA a 899

--- git-push.bat
+++ git-push.bat
@@ -2,7 +2,7 @@
 cd /d "%~dp0"
 del /f /q .git\index.lock 2>nul
 git add -A
-git commit -m "landing: cubre toda la plataforma - asistente IA Aura, lealtad y monedero, marketing, modulos restaurante y gimnasio, bundles Todo+RES y Todo+GYM, tabla comparativa ampliada"
+git commit -m "Precios: Agenda baja a 149 y bundle Todo ALGORA a 899"
 git push origin main
 echo.
 echo Done! Press any key to close...
