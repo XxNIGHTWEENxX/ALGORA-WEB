e9ccfc7a391f79768fef3eee2e6d5cb6cac8dc0c
XxNIGHTWEENxX <alan.garciagon@alumno.buap.mx>
2026-08-13 17:38:53 -0600

    SEO: datos estructurados Organization con logo y WebSite mas og site_name para que Google muestre el logo de marca

--- git-push.bat
+++ git-push.bat
@@ -2,7 +2,7 @@
 cd /d "%~dp0"
 del /f /q .git\index.lock 2>nul
 git add -A
-git commit -m "Precios: Agenda baja a 149 y bundle Todo ALGORA a 899"
+git commit -m "SEO: datos estructurados Organization con logo y WebSite mas og site_name para que Google muestre el logo de marca"
 git push origin main
 echo.
 echo Done! Press any key to close...
