093999f89a7a87580223b315d657eaae842908ee
Alan Gongora <alan.garciagon@alumno.buap.mx>
2026-08-24 03:31:55 -0600

    Delete git-push.bat

--- git-push.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-del /f /q .git\index.lock 2>nul
-git add -A
-git commit -m "ALGORA TECH: home corporativo con logo en barra y pie; productos ALGORA POS Florinia NoFollow y servicios; landing POS en pos.html; sitios.html; sitemap"
-git push origin main
-echo.
-echo Done! Press any key to close...
-pause >nul
